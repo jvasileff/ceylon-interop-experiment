50420af73e047516975aedbe7f48b5c7695ecc38
John Vasileff <john@vasileff.com>
2014-11-02 15:29:38 -0500

    works as a plain Java app w/o "setupMetamodel"

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# works with initial hello world project
 java -cp $(ceylon classpath --rep aether com.example.helloworld/1.0.0) \
-    com.redhat.ceylon.compiler.java.runtime.Main \
-    com.example.helloworld/1.0.0 \
-    com.example.helloworld.run_
-
-
+    com.example.helloworld.Main
---
+++ source/com/example/helloworld/Main.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class Main {
+    public static final void main(String[] args) {
+        run_.run();
+    }
+}
+
