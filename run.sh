8f33398683d1a113e66226bcb4ad4152ef65c50f
John Vasileff <john@vasileff.com>
2014-11-05 15:32:03 -0500

    jar w/pom.xml helps, but not a final solution
    Adding a pom for the spring-context dependency solves the initial problem, but
    then, one of its dependencies, spring-beans, causes the same error.  Adding the
    pom.xml for spring-beans isn't sufficent.
    
    Adding spring-beans to module.ceylon may help, but we shouldn't have to do
    that. And, spring-beans is spring-context's dependency, not necessarily a
    dependency of our hello-world module.
    
    The final exception, with both pom.xml's available, is:
    
    Exception in thread "main" java.lang.RuntimeException: com.redhat.ceylon.compiler.java.runtime.Main$ClassPath$ModuleNotFoundException: Module org.springframework:spring-beans/4.0.3.RELEASE not found
            at com.redhat.ceylon.compiler.java.runtime.Main.registerInMetamodel(Main.java:546)
            at com.redhat.ceylon.compiler.java.runtime.Main.registerInMetamodel(Main.java:553)
            at com.redhat.ceylon.compiler.java.runtime.Main.registerInMetamodel(Main.java:553)
            at com.redhat.ceylon.compiler.java.runtime.Main.setupMetamodel(Main.java:528)
            at com.redhat.ceylon.compiler.java.runtime.Main.runModule(Main.java:474)
            at com.redhat.ceylon.compiler.java.runtime.Main.main(Main.java:580)
    Caused by: com.redhat.ceylon.compiler.java.runtime.Main$ClassPath$ModuleNotFoundException: Module org.springframework:spring-beans/4.0.3.RELEASE not found
            at com.redhat.ceylon.compiler.java.runtime.Main$ClassPath.loadModule(Main.java:297)
            at com.redhat.ceylon.compiler.java.runtime.Main.registerInMetamodel(Main.java:542)
            ... 5 more

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # works with initial hello world project
-java -cp $(ceylon classpath --rep aether com.example.helloworld/1.0.0) \
+java -cp "lib.jar:$(ceylon classpath --rep aether com.example.helloworld/1.0.0)" \
     com.redhat.ceylon.compiler.java.runtime.Main \
     com.example.helloworld/1.0.0 \
     com.example.helloworld.run_
