9e2bb5301ce928ee30d813d2189bc2d799b2788a
John Vasileff <john@vasileff.com>
2014-11-05 14:48:56 -0500

    attempted fix by including jsr305.jar in cp
    This doesn't work either, but possibly due to something with jsr305's pom.xml,
    as retrieved from Maven central.
    
    This commit adds a custom jsr305-with-pom.jar since the distribution jar does
    not include pom.xml.
    
    The new error is:
    
    Exception in thread "main" java.lang.IllegalArgumentException: No dependencies were set for resolution
            at org.jboss.shrinkwrap.resolver.impl.maven.util.Validate.notEmpty(Validate.java:78)
            at org.jboss.shrinkwrap.resolver.impl.maven.MavenStrategyStageBaseImpl.using(MavenStrategyStageBaseImpl.java:69)
            at org.jboss.shrinkwrap.resolver.impl.maven.MavenStrategyStageBaseImpl.using(MavenStrategyStageBaseImpl.java:40)
            at com.redhat.ceylon.cmr.maven.AetherUtils.getDependencies(AetherUtils.java:83)
            at com.redhat.ceylon.cmr.maven.AetherUtils.getDependencies(AetherUtils.java:91)
            at com.redhat.ceylon.cmr.maven.MavenDependencyResolver.resolveFromInputStream(MavenDependencyResolver.java:87)
            at com.redhat.ceylon.compiler.java.runtime.Main$ClassPath.loadFromResolver(Main.java:426)
            at com.redhat.ceylon.compiler.java.runtime.Main$ClassPath.loadMavenJar(Main.java:442)
            at com.redhat.ceylon.compiler.java.runtime.Main$ClassPath.loadJar(Main.java:311)
            at com.redhat.ceylon.compiler.java.runtime.Main$ClassPath.loadModule(Main.java:283)
            at com.redhat.ceylon.compiler.java.runtime.Main.registerInMetamodel(Main.java:542)
            at com.redhat.ceylon.compiler.java.runtime.Main.registerInMetamodel(Main.java:553)
            at com.redhat.ceylon.compiler.java.runtime.Main.registerInMetamodel(Main.java:553)
            at com.redhat.ceylon.compiler.java.runtime.Main.setupMetamodel(Main.java:528)
            at com.redhat.ceylon.compiler.java.runtime.Main.runModule(Main.java:474)
            at com.redhat.ceylon.compiler.java.runtime.Main.main(Main.java:580)

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # works with initial hello world project
-java -cp $(ceylon classpath --rep aether com.example.helloworld/1.0.0) \
+java -cp "jsr305-with-pom.jar:$(ceylon classpath --rep aether com.example.helloworld/1.0.0)" \
     com.redhat.ceylon.compiler.java.runtime.Main \
     com.example.helloworld/1.0.0 \
     com.example.helloworld.run_
